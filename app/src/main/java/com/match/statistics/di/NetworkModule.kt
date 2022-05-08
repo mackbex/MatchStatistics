f6f31e40106be60609d3fcd441dab885252578b4
Tom Kim <tom.kim@3i.ai>
2022-05-09 02:33:23 +0900

    - add retrofit compatiable process from API 19

--- app/src/main/java/com/match/statistics/di/NetworkModule.kt
+++ app/src/main/java/com/match/statistics/di/NetworkModule.kt
@@ -2,44 +2,48 @@ package com.match.statistics.di
 
 import com.match.statistics.BuildConfig
 import com.match.statistics.data.source.remote.service.LoLService
+import com.match.statistics.data.source.InternalSSLSocketFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.CipherSuite.Companion.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256
-import okhttp3.CipherSuite.Companion.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
-import okhttp3.CipherSuite.Companion.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
 import okhttp3.ConnectionSpec
 import okhttp3.OkHttpClient
-import okhttp3.TlsVersion
 import okhttp3.logging.HttpLoggingInterceptor
-import okhttp3.logging.HttpLoggingInterceptor.*
+import okhttp3.logging.HttpLoggingInterceptor.Level
+import org.conscrypt.Conscrypt
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.Security
 import java.util.*
 import java.util.concurrent.TimeUnit
 import javax.inject.Named
 import javax.inject.Singleton
+import javax.net.ssl.SSLContext
+import javax.net.ssl.X509TrustManager
 
 
 @Module
 @InstallIn(SingletonComponent::class)
+
 object NetworkModule {
 
     @Singleton
     @Provides
     fun provideOkHttpClient() : OkHttpClient {
 
-        val spec: ConnectionSpec = ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
-            .tlsVersions(TlsVersion.TLS_1_2)
-            .cipherSuites(
-                TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-                TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-                TLS_DHE_RSA_WITH_AES_128_GCM_SHA256
-            )
+
+        Security.insertProviderAt(Conscrypt.newProvider(), 1)
+
+        val tm: X509TrustManager = Conscrypt.getDefaultX509TrustManager()
+        val sslContext = SSLContext.getInstance("TLS", "Conscrypt")
+        sslContext.init(null, arrayOf(tm), null)
+
+        val spec = ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
             .build()
 
         val client = OkHttpClient.Builder()
+            .sslSocketFactory(InternalSSLSocketFactory(sslContext.socketFactory), tm)
             .connectionSpecs(Collections.singletonList(spec))
             .readTimeout(10, TimeUnit.SECONDS)
             .connectTimeout(10, TimeUnit.SECONDS)
